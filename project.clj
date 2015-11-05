0e090e2aecfacec523452330755ad7c9ef0285be
Tom Bombabil <amanibhavam@destructuring.org>
2015-11-04 23:01:22 -0800

    remove license from project.clj, default to LICENSE file

--- project.clj
+++ project.clj
@@ -1,8 +1,5 @@
 (defproject memtest "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "memtest game"
 
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [ring-server "0.4.0"]
